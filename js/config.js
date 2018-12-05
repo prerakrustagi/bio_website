b98c051d60780d33d953c2c1588cca3a0f607ee7
Prerak Rustagi <prerak1992@gmail.com>
2018-12-05 12:58:34 -0600

    initial commit

---
+++ js/config.js
@@ -0,0 +1,115 @@
+/* Google MAP Settings*/
+  var lat= 18.538844;  //Change the value with your address Latitude
+  var lng= 73.900301;  //Change the value with your address Longitude
+
+//Skill Chart Data
+var skillData = {
+	skill1: [
+		{
+			title: "ASP.Net MVC 80%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '80%'
+		},
+		{
+			title: "HTML 70%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '70%'
+		},
+		{
+			title: "CSS 70%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '70%'
+		},
+		{
+			title: "JavaScript 70%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '70%'
+		},
+		{
+			title: "JQuery 60%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '60%'
+		}
+	],
+	skill2: [
+		{
+			title: "C# (.Net) 80%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '80%'
+		},
+		{
+			title: "Scala 60%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '60%'
+		},
+		{
+			title: "MongoDb 75%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '75%'
+		},
+		{
+			title: "ElasticSearch 80%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '80%'
+		},
+		{
+			title: "ASP Dot Net Web API 70%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '70%'
+		},
+    	{
+			title: "MySql 50%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '50%'
+		},
+		{
+			title: "Amazon Redshift 50%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '50%'
+		}
+	],
+  skill3: [
+		{
+			title: "Innovation 90%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '90%'
+		},
+		{
+			title: "Leadership 85%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '85%'
+		},
+    	{
+			title: "Bike&nbsp;Riding 90%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '90%'
+		},
+		{
+			title: "Travelling 95%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '95%'
+		},
+		{
+			title: "Reading 75%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '75%'
+		}
+	]
+}
